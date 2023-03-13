c590a4c84e926e48afd55f033cd6edda2519ff65
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-13 14:15:19 +0100

    Creati array diversi per tipologia

--- js/main.js
+++ js/main.js
@@ -115,13 +115,56 @@ const icons = [
 
 // collegamenti al DOM
 const contentDom = document.getElementById("content");
+const typeFilterDom = document.getElementById("type-filter");
+let chosenType = typeFilterDom.value;
+console.log(chosenType);
 
 icons.forEach((element) => {
     const boxIcon = `<div class="box">
                         <i class="fa-solid ${element.prefix}${element.name}" style="color: ${element.color}"></i>
                         <h4>${element.name}</h4>
                      </div>`;
-    console.log(boxIcon);
+    // console.log(boxIcon);
 
     contentDom.innerHTML += boxIcon;
+})
+
+typeFilterDom.addEventListener("change", function(){
+    chosenType = typeFilterDom.value;
+    console.log(chosenType);
+
+    if (chosenType == "animal") {
+        const animals = icons.filter((icon) => {
+            if (icon.type == "animal") {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        console.log(animals);
+    } 
+
+    if (chosenType == "vegetable") {
+        const vegetables = icons.filter((icon) => {
+            if (icon.type == "vegetable") {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        console.log(vegetables);
+    } 
+
+    if (chosenType == "user") {
+        const users = icons.filter((icon) => {
+            if (icon.type == "user") {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        console.log(users);
+    } 
+
+
 })
